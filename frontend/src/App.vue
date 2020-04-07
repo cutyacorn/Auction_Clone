97890244db2e248bf13906f32e1a778c47215d3b
cutyacorn <cutyacorn@outlook.com>
2020-04-07 23:19:53 +0900

    vue router add

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,142 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="main_title">
+      <div class="title_menu">
+        <ul>
+          <li><router-link to="/login">로그인</router-link></li>
+          <li><router-link to="/join">회원가입</router-link></li>
+        </ul>
+      </div>
+      <div class ="title_auction">
+        <div class="title_item">
+          <router-link to="/"><img class="img_size" src="https://pics.auction.co.kr/pc/renewal/common/header/logo_@2x.png" alt="AUCTION"></router-link>
+        </div>
+        <div class="search_inner">
+          <input type="submit" value="검색" class="search_btn_ok">
+        </div>
+      </div>
+    </div>
+
+    <div class="gnb_bar">
+      <nav class="gnb">
+        <ul class="gnb_list">
+          <li><a href=""><strong>☰</strong> 전체 카테고리</a></li>
+          <li> | </li>
+          <li><a href="">쿠폰포인트</a></li>
+          <li> | </li>
+          <li><a href="">옥션베스트</a></li>
+          <li> | </li>
+          <li><a href="">스마일배송</a></li>
+          <li> | </li>
+          <li><a href="">스마일클럽</a></li>
+        </ul>
+      </nav>
+    </div>
+    <join/>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+a{
+  text-decoration: none;
+  color:gray
+}
+.main_title{
+  min-width: 1000px;
+}
+.title_menu{
+  position: relative;
+  height: 20px;
+  margin: 0 auto;
+  width: 1000px;
+}
+.title_menu > ul {
+  margin: 0;
+  padding: 0;
+  float: right;
+}
+.title_menu > ul > li{
+  list-style-type: none;
+  display: inline;
+  margin-left: 5px;
+  font-size: 12px;
+}
+.title_auction{
+  width: 930px;
+  height: 80px;
+  margin: 0 auto;
+}
+.img_size{
+  width: 200px;
+}
+.title_item{
+  /*display: inline-block;*/
+  float:left;
+}
+.search_inner {
+    /*display: inline-block;*/
+    float:left;
+    position: relative;
+    width: 400px;
+    height: 44px;
+    border-bottom: 4px solid #e63740;
+}
+.search_btn_ok {
+    position: absolute;
+    right: 0;
+    top: 12px;
+    display: block;
+    width: 22px;
+    height: 29px;
+    border: none;
+    background: url(//pics.auction.co.kr/pc/common/header/header.png) -212px -90px no-repeat;
+    text-indent: -10000em;
+    overflow: hidden;
+    cursor: pointer;
+    -webkit-appearance: none;
+    -webkit-border-radius: 0;
+    box-sizing: border-box;
+}
+.gnb_bar {
+    min-width: 1000px;
+    border-top: 1px solid #ededed;
+    border-bottom: 2px solid #242424;
+}
+.gnb {
+    width: 1000px;
+    height: 48px;
+    margin: 0 auto;
+}
+.gnb_list {
+    float: left;
+    width: 750px;
+    zoom: 1;
+}
+.gnb_list li:first-child {
+    margin-left: 0;
+    top: -5px;
+}
+.gnb_list > li {
+    top: -2px;
+    list-style-type: none;
+    position: relative;
+    float: left;
+    margin-left: 20px;
+}
+.gnb_list li a {
+    color: black;
+    font-size: 16px;
+    font-weight: bold;
+}
+.gnb_list li strong{
+    font-size: 18px;
+    font-weight: bold;
 }
 </style>
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="login">
+    <h1>This is a login page</h1>
+    <Footer/>
+  </div>
+</template>
+<script>
+import Footer from '@/components/Footer.vue'
+export default {
+  name: 'Home',
+  components: {
+    Footer
+  }
+}
+</script>
