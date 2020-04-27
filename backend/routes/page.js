040bb4479ae5537bd45fda1e02cd3dd74672f3f9
tkdwls9277 <tkdwls9277@naver.com>
2020-04-27 22:55:37 +0900

    제로초 로그인화면

--- backend/routes/page.js
+++ backend/routes/page.js
@@ -1,28 +1,47 @@
 const express = require('express');
+// const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+// const { Post, User } = require('../models');
 
 const router = express.Router();
 
-//'/profile','/join','/' 3개의 페이지로 구성 
+// router.get('/profile', isLoggedIn, (req, res) => {
+//   res.render('profile', { title: '내 정보 - NodeBird', user: req.user });
+// });
 
-router.get('/profile',(req, res)=>{
-    res.render('profile',{title:'내 정보 - Nodebird',user:null});
+router.get('/profile', (req, res) => {
+    res.render('profile', { title: '내 정보 - NodeBird', user: null });
 });
 
-router.get('/join',(req,res)=>{
-    res.render('join',{
-        title:'회원가입 - NodeBird',
-        user:null,
-        joinError:req.flash('joinError'),
-    });
+router.get('/join', (req, res) => {
+  res.render('join', {
+    title: '회원가입 - NodeBird',
+    user: null,
+    joinError: req.flash('joinError'),
+  });
 });
 
-router.get('/',(req,res,next)=>{
-    res.render('main',{
-        title:'NodeBird',
-        twints:[],
-        user:null,
-        loginError:req.flash('loginError'),
-    });
-});
+router.get('/', (req, res, next) => {
+//   Post.findAll({
+//     include: {
+//       model: User,
+//       attributes: ['id', 'nick'],
+//     },
+//     order: [['createdAt', 'DESC']],
+//   })
+//     .then((posts) => {
+      res.render('main', {
+        title: 'NodeBird',
+        // twits: posts,
+        twits: [],
+        // user: req.user,
+        user: null,
+        loginError: req.flash('loginError'),
+      });
+    })
+    // .catch((error) => {
+    //   console.error(error);
+    //   next(error);
+    // });
+// });
 
 module.exports = router;
