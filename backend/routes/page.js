0229f83c358cf698007d64fa3f0ec82f499f4f90
tkdwls9277 <tkdwls9277@naver.com>
2020-05-05 22:59:39 +0900

    post upload

--- backend/routes/page.js
+++ backend/routes/page.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
-// const { Post, User } = require('../models');
+const { Post, User } = require('../models');
 
 const router = express.Router();
 
@@ -17,27 +17,25 @@ router.get('/join', isNotLoggedIn, (req, res) => {
 });
 
 router.get('/', (req, res, next) => {
-//   Post.findAll({
-//     include: {
-//       model: User,
-//       attributes: ['id', 'nick'],
-//     },
-//     order: [['createdAt', 'DESC']],
-//   })
-//     .then((posts) => {
-      res.render('main', {
-        title: 'NodeBird',
-        // twits: posts,
-        twits: [],
-        user: req.user,
-        user: null,
-        loginError: req.flash('loginError'),
-      });
-    })
-    // .catch((error) => {
-    //   console.error(error);
-    //   next(error);
-    // });
-// });
+  Post.findAll({
+    include:{
+      model:User,
+      attributes:['id','nick'],
+    },
+    order:[['createdAt', 'DESC']],
+  })
+  .then((posts)=>{
+    res.render('main', {
+      title: 'NodeBird',
+      twits: posts,
+      user: req.user,
+      loginError: req.flash('loginError'),
+    });
+  })
+  .catch((error)=>{
+    console.error(error);
+    next(error);
+  });
+});
 
 module.exports = router;
